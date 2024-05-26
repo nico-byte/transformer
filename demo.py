19c658ab62c07a303b039f430efe53645d5b9b0e
Nico Fuchs <fuchs19635@hs-ansbach.de>
2024-05-26 21:42:56 +0200

    added info to input and output field of gradio demo

--- demo.py
+++ demo.py
@@ -12,8 +12,8 @@ def translate(sequence):
 
 demo = gr.Interface(
     fn=translate,
-    inputs=[gr.Textbox(label="Input", lines=1)],
-    outputs=[gr.Textbox(label="Output", lines=1)],
+    inputs=[gr.Textbox(label="Input", lines=1, info="Enter an english sentence.")],
+    outputs=[gr.Textbox(label="Output", lines=1, info="The translated sentence in german.")],
 )
 
 demo.launch()
